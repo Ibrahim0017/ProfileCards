f0877c3a81f2424a36ec1875b2d2ea8d0de2c220
Ibrahim0017 <ibrahim72336@gmail.com>
2024-09-09 10:57:30 +0100

    first commmit

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import Image from './assets/143272.jpg'
+import { FiPlus } from "react-icons/fi";
+import { SiGmail } from "react-icons/si";
+import { GrFacebookOption } from "react-icons/gr";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaPinterest } from "react-icons/fa";
 
 const Cards = () => {
   return (
     <>
         <Container>
             <Up>
-                <Circle></Circle>
+                <Circle> <FiPlus />
+</Circle>
             </Up>
             <Down>
                 <ImageHolder>
@@ -16,8 +23,17 @@ const Cards = () => {
                 <Name>Samuel Joshua</Name>
                 <Text>Alice works from home, but sometimes she commute to co-working space,
           where she has the opportunity to share </Text>
+
+          <IconDiv>
+            <SiGmail />
+            <GrFacebookOption />
+            <FaLinkedinIn />
+            <FaXTwitter />
+            <FaPinterest />
+
+            </IconDiv>
             </Down>
-            <IconDiv></IconDiv>
+            
         </Container>
     </>
   )
@@ -28,7 +44,15 @@ export default Cards
 
 
 const IconDiv = styled.div`
-
+width: 60%;
+height: 20px;
+display: flex;
+align-items: center;
+justify-content: center;
+justify-content: space-between;
+font-size: 20px;
+color: #b7b7b7;
+margin: 30px;
 `
 
 const Text = styled.h5`
@@ -75,6 +99,11 @@ position: relative;
 top: -10px;
 right: -10px;
 box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 20px;
+color: white;
 `
 
 const Up = styled.div`
